e82f5152f10c48fcb6a99acfba9e9cecddeaa727
T.D. Mees <t.d.mees@uu.nl>
2019-01-10 16:09:55 +0100

    Replaced all url calls with path calls

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.urls import path
+
 from .views import HomeView
 
 
 app_name = 'main'
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^$', HomeView.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', HomeView.as_view(), name='home'),
 
 ]
