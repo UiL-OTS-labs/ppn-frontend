a89ada77450856220969710886ef9bb45b755677
T.D. Mees <t.d.mees@uu.nl>
2019-01-18 10:13:02 +0100

    Updated translations and added a way to open/close experiments as leader

--- leader/urls.py
+++ leader/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import ExperimentsView, ProfileView
+from .views import ExperimentsView, ProfileView, SwitchExperimentOpenView
 
 
 app_name = 'leader'
 
 urlpatterns = [
     path('', ExperimentsView.as_view(), name='experiments'),
+    path('experiment/<int:experiment>/switch_open',
+         SwitchExperimentOpenView.as_view(), name='experiment_switch_open'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- leader/views.py
+++ leader/views.py
@@ -1,11 +1,18 @@
 from braces import views as braces
 from django.conf import settings
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy as reverse
 from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _
 from django.views import generic
+from uil.core.views import RedirectActionView
+from uil.core.views.mixins import RedirectSuccessMessageMixin
 
 from api.auth.models import RemoteApiUser
-from api.resources import Leader, LeaderExperiments
+from api.exceptions import ApiError
+from api.resources import Leader, LeaderExperiments, SwitchExperimentOpen, \
+    ChangeLeader
 from leader.forms import ChangeProfileForm
 from leader.models import LeaderPhoto
 
@@ -25,13 +32,46 @@ class ExperimentsView(braces.RecentLoginRequiredMixin,
         return context
 
 
+class SwitchExperimentOpenView(braces.RecentLoginRequiredMixin,
+                               braces.GroupRequiredMixin,
+                               RedirectSuccessMessageMixin,
+                               RedirectActionView):
+    group_required = [settings.GROUPS_LEADER]
+    pattern_name = 'leader:experiments'
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse(self.pattern_name)
+
+    def action(self, request):
+        experiment = self.kwargs.pop('experiment')
+
+        try:
+            res = SwitchExperimentOpen.client.get(experiment=experiment)
+
+            if res.success:
+                if res.open:
+                    self.success_message = _('experiment:message:open')
+                else:
+                    self.success_message = _('experiment:message:closed')
+            else:
+                self.success_message = _('experiment:message:fail')
+
+        except ApiError as e:
+            if e.status_code == 403:
+                raise PermissionDenied
+
+            self.success_message = _('experiment:message:fail')
+
+
 class ProfileView(braces.RecentLoginRequiredMixin,
                   braces.GroupRequiredMixin,
+                  SuccessMessageMixin,
                   generic.FormView):
 
     template_name = 'leader/profile.html'
     form_class = ChangeProfileForm
-    success_url = reverse('leader:profile')
+    success_url = reverse('leader:experiments')
+    success_message = _('profile:message:updated')
     group_required = [settings.GROUPS_LEADER]
 
     @cached_property
@@ -67,5 +107,10 @@ class ProfileView(braces.RecentLoginRequiredMixin,
 
         photo.photo = photo_val
         photo.save()
+
+        change_leader = ChangeLeader()
+        change_leader.name = values['name']
+        change_leader.phonenumber = values['phone']
+        change_leader.put()
 
         return super(ProfileView, self).form_valid(form)
