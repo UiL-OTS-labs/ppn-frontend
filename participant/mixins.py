79bb4e66ff0ee28108a62a0c540aaf215c026561
T.D. Mees <t.d.mees@uu.nl>
2019-01-18 12:02:10 +0100

    Added access control mixins for participants views

---
+++ participant/mixins.py
@@ -0,0 +1,30 @@
+from braces.views import GroupRequiredMixin
+from django.conf import settings
+from django.contrib.auth.mixins import AccessMixin
+
+
+class ParticipantRequiredMixin(GroupRequiredMixin):
+    """This mixin requires that a user is also a participant. Shorthand mixin"""
+    group_required = [settings.GROUPS_PARTICIPANT]
+
+
+class ParticipantForbiddenMixin(AccessMixin):
+    """This mixin will allow anonymous users and users that are NOT in the
+    participant group to view this view.
+
+    It's basically a ¬ for the ParticipantRequiredMixin
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        user = request.user
+
+        if not user.is_anonymous:
+            if user.groups.filter(name=settings.GROUPS_PARTICIPANT).exists():
+                return self.handle_no_permission(request)
+
+        return super(ParticipantForbiddenMixin, self).dispatch(
+            request,
+            *args,
+            **kwargs
+        )
